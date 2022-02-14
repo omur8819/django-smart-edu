85af3fb54e4f0c9f829da017d86812657ddfeb74
omur8819 <omur8819@gmail.com>
2022-02-14 23:46:38 +0300

    fix: course model created

---
+++ smartedu_con/courses/models.py
@@ -0,0 +1,9 @@
+from email.policy import default
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Kurs Adı", help_text="Kurs adını yazınız")
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
